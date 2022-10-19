71115cf2f02fa314e8d7344361c63e2eebd4deb0
Lugges991 <29121982+Lugges991@users.noreply.github.com>
2022-10-19 15:57:20 +0200

    implemented 2cc3d

---
+++ interMIA/models/TwoCC3D.py
@@ -0,0 +1,68 @@
+import torch
+from torch import nn
+from torchinfo import summary
+
+
+class Block(nn.Module):
+    def __init__(self, in_channels, out_channels, p_kernel=(2, 2, 2)):
+        super().__init__()
+
+        c_kernels = [(3, 3, 3) for i in range(len(out_channels))]
+
+        self.layer_list = []
+        for i, (in_c, out_c) in enumerate(zip(in_channels, out_channels)):
+            self.layer_list.append(
+                nn.Conv3d(in_c, out_c, kernel_size=c_kernels[i]))
+            self.layer_list.append(nn.MaxPool3d(
+                kernel_size=p_kernel, stride=1))
+
+        self.layer_list = nn.ModuleList(self.layer_list)
+
+    def forward(self, x):
+        for layer in self.layer_list:
+            x = layer(x)
+        return x
+
+
+class TwoCC3D(nn.Module):
+    def __init__(self, in_channels=2, n_classes=2):
+        super().__init__()
+
+        self.block1 = Block(in_channels=[in_channels], out_channels=[
+                            32])
+        self.do1 = nn.Dropout(p=0.5)
+        self.block2 = Block(in_channels=[32], out_channels=[64])
+        self.do2 = nn.Dropout(p=0.5)
+        self.block3 = Block(in_channels=[64, 128], out_channels=[128, 128])
+        self.do3 = nn.Dropout(p=0.65)
+        self.block4 = Block(in_channels=[128, 128], out_channels=[128, 128])
+        self.do4 = nn.Dropout(p=0.65)
+        self.flat = nn.Flatten()
+        self.fc6 = nn.Linear(128 * 14**3, 256)
+        self.fc7 = nn.Linear(256, 256)
+        self.fc8 = nn.Linear(256, n_classes)
+        self.sm = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.do1(x)
+        x = self.block2(x)
+        x = self.do2(x)
+        x = self.block3(x)
+        x = self.do3(x)
+        x = self.block4(x)
+        x = self.do4(x)
+        breakpoint()
+        x = self.flat(x)
+
+        x = self.fc6(x)
+        x = self.fc7(x)
+        x = self.fc8(x)
+        x = self.sm(x)
+        return x
+
+
+if __name__ == "__main__":
+    model = TwoCC3D().to("cuda")
+    inp = torch.rand((1, 2, 32, 32, 32)).to("cuda")
+    summary(model, input_data=inp)
--- interMIA/utils/preprocessing_2c.py
+++ interMIA/utils/preprocessing_2c.py
@@ -1,1 +1,4 @@
 """Script that takes FSL preprocessed data, performs temporal slicing, averages over temporal slice, calculates std for temporal slice and stores as 2 channel 3D volume"""
+import pandas as pd
+from pathlib import Path
+import nibabel as nib
